bed14550fbbaaa466f2323f1b2046b129d970c1e
AniketSaini0 <yashsaini252000@gmail.com>
2025-03-25 13:34:45 +0530

    initial commit

---
+++ 02ReactTailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-400	bg-gray-900 rounded-md p-4">
+        This is Vite-React-Tailwind app
+      </h1>
+    </>
+  );
+}
+
+export default App;
